ec4d1ac1d956044688a9c9e6e245e81f846e9962
Saikishor Rasala <rasalasaikishor@gmail.com>
2023-08-02 18:11:41 +0530

    made some changes in app.js and index.js

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,17 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+
+const myFirstElement = <a href='https://saikishor164.github.io'><h1>saikishor Rasala</h1> </a>
+// const mysecondelement = <h1>saikishor</h1>
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+root.render(myFirstElement);
+// root.render(mysecondelement);
